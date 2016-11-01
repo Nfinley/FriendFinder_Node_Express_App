ad9a6e19f0056e2c3d17e21f401c83b586ce47fb
Nigel Finley <nfinleymusic@gmail.com>
2016-10-31 20:42:33 -0500

    fixed css issues and updated http to https

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@
 
 var users = [{
     "name": "Ryan",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/v1477628283/imkage_kdx490.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/v1477628283/imkage_kdx490.jpg",
     "scores": [
         5,
         1,
@@ -20,7 +20,7 @@ var users = [{
     "scoreSum": 31
 }, {
     "name": "Jackie",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_767/v1477628490/jackie_vfipwl.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_767/v1477628490/jackie_vfipwl.jpg",
     "scores": [
         2,
         5,
@@ -36,7 +36,7 @@ var users = [{
     "scoreSum": 33
 },{
     "name": "Seth",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_863/v1477628350/Seth-Rogen_hblf9d.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_863/v1477628350/Seth-Rogen_hblf9d.jpg",
     "scores": [
         3,
         4,
@@ -52,7 +52,7 @@ var users = [{
     "scoreSum": 29
 },{
     "name": "Audrey",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/v1477627727/audrey-hepburn-399789_640_katc2e.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/v1477627727/audrey-hepburn-399789_640_katc2e.jpg",
     "scores": [
         2,
         3,
@@ -68,7 +68,7 @@ var users = [{
     "scoreSum": 32
 },{
     "name": "George",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_671/v1477628118/george-clooney_tlx0s7.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/c_scale,w_671/v1477628118/george-clooney_tlx0s7.jpg",
     "scores": [
         4,
         2,
@@ -84,7 +84,7 @@ var users = [{
     "scoreSum": 25
 },{
     "name": "Chelsea",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/v1477628615/chelsea-handler_lnpagi.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/v1477628615/chelsea-handler_lnpagi.jpg",
     "scores": [
         3,
         2,
@@ -100,7 +100,7 @@ var users = [{
     "scoreSum": 24
 },{
     "name": "Marilyn",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/v1477627911/marilyn-monroe-399194_640_p0hocf.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/v1477627911/marilyn-monroe-399194_640_p0hocf.jpg",
     "scores": [
         4,
         3,
@@ -116,7 +116,7 @@ var users = [{
     "scoreSum": 26
 },{
     "name": "Brad",
-    "photo": "http://res.cloudinary.com/thefinleycode/image/upload/v1477627586/brad-pitt-164880_640_cbz0hg.jpg",
+    "photo": "https://res.cloudinary.com/thefinleycode/image/upload/v1477627586/brad-pitt-164880_640_cbz0hg.jpg",
     "scores": [
         1,
         2,
